dc2b4c3881641a310bec32e5e6a216a1b352d063
Kendall Weistroffer <Lockwood@Kendalls-MacBook-Pro.local>
2015-11-02 15:06:13 -0700

    Adding Ass Class

---
+++ src/sample/Association.java
@@ -0,0 +1,28 @@
+package sample;
+
+public class Association {
+
+	public int subjectID;
+	public Class assClass;
+	
+	
+	public Association(){
+		
+	}
+	
+	public int getSubjectID() {
+		return subjectID;
+	}
+	public void setSubjectID(int subjectID) {
+		this.subjectID = subjectID;
+	}
+	public Class getAssClass() {
+		return assClass;
+	}
+	public void setAssClass(Class assClass) {
+		this.assClass = assClass;
+	}
+	
+	
+	
+}
