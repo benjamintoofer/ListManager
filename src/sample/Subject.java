a20d476dd12a000cc12e5ad5426c30c779c2006f
Benjamin Toofer <benjamintoofer@gmail.com>
2015-11-01 17:07:11 -0700

    Added tree view and menubar to ui and added Subject class

---
+++ src/sample/Subject.java
@@ -0,0 +1,61 @@
+package sample;
+
+import java.util.ArrayList;
+
+/**
+ * Created by benjamintoofer on 11/1/15.
+ */
+public class Subject {
+
+    private String name;
+    private String description;
+    private ArrayList<Subject> subTopicList;
+    private ArrayList<String> learningOutcomeList;
+
+
+    public Subject(){
+
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    public ArrayList<Subject> getSubTopicList()
+    {
+        return subTopicList;
+    }
+
+    public void addSubTopic(Subject addSubject)
+    {
+        this.subTopicList.add(addSubject);
+    }
+
+    public ArrayList<String> getLearningOutcomeList()
+    {
+        return learningOutcomeList;
+    }
+
+    public void addLearningOutcome(String learnOut){
+
+        this.learningOutcomeList.add(learnOut);
+
+    }
+}
