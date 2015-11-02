316140521dc312a0b535cb1c50c5f59c370ebff8
Benjamin Toofer <benjamintoofer@gmail.com>
2015-11-02 08:03:42 -0700

    Added ClassListView class

--- src/sample/ClassListView.java
+++ src/sample/ClassListView.java
@@ -6,15 +6,19 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 
 public class ClassListView extends VBox{
 
     private  ListView<String> listView;
-    private MenuBar courseMenuBar;
+    private HBox buttonBox;
 
     public ClassListView(){
 
@@ -25,21 +29,34 @@ public class ClassListView extends VBox{
 
         //Instantiate listView
         listView = new ListView<>();
+        listView.setEditable(true);
         listView.setPrefSize(300,900);
         ObservableList<String> tempItems = FXCollections.observableArrayList("COMP2730", "COMP2400", "COMP3351", "COMP3381");
         listView.setItems(tempItems);
         listView.setCellFactory((ListView<String> l)->new CustomClassListCell());
 
 
-        //Instantiate courseMenuBar
-        courseMenuBar = new MenuBar();
-        Menu addCourseButton = new Menu("+");
-        Menu removeCourseButton = new Menu("-");
-        courseMenuBar.getMenus().addAll(addCourseButton,removeCourseButton);
+
+        //Instantiate add and remove course buttons
+        buttonBox = new HBox();
+        Button addCourseButton = new Button("+");
+        addCourseButton.setId("add_course_button");
+        addCourseButton.setOnAction(new AddRemoveClassHandler());
+        Button removeCourseButton = new Button("-");
+        removeCourseButton.setId("remove_course_button");
+        removeCourseButton.setOnAction(new AddRemoveClassHandler());
+        buttonBox.getChildren().addAll(addCourseButton,removeCourseButton);
 
 
-        this.getChildren().addAll(listView,courseMenuBar);
+
+
+        this.getChildren().addAll(listView,buttonBox);
     }
+
+    private boolean removeCourse(){
+        return true;
+    }
+
 
     private class CustomClassListCell extends ListCell<String>{
 
@@ -51,9 +68,8 @@ public class ClassListView extends VBox{
 
         @Override
         public void startEdit(){
-            System.out.println("Start edit 1");
+
             super.startEdit();
-            System.out.println("Start edit");
             if(textField == null)
                 createTextField();
 
@@ -75,7 +91,7 @@ public class ClassListView extends VBox{
         @Override
         public void updateItem(String item, boolean empty){
 
-            super.updateItem(item,empty);
+            super.updateItem(item, empty);
 
             if(empty){
                 setText(null);
@@ -88,11 +104,11 @@ public class ClassListView extends VBox{
                     setText(null);
                 }else{
                     setText(getString());
-                    setGraphic(getGraphic());
+                    setGraphic(null);
                 }
             }
-            super.updateListView(this.getListView());
         }
+
 
         private void createTextField(){
 
@@ -109,5 +125,27 @@ public class ClassListView extends VBox{
         private String getString(){
             return (getItem() == null ? "" : getItem().toString());
         }
+    }
+
+
+
+
+    private class AddRemoveClassHandler implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent e){
+
+            System.out.println("Handling");
+            if(e.getTarget().getClass().toString().equals("class javafx.scene.control.Button")){
+
+                if(((Button)e.getTarget()).getId().equals("add_course_button")){
+                    System.out.println("Adding course");
+                }
+                if(((Button)e.getTarget()).getId().equals("remove_course_button")){
+                    System.out.println("Removing course");
+                }
+            }
+        }
+
     }
 }
